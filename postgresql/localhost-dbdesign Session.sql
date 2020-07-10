c2faa515516bc1c543f5ba6f717029c4a20ff1a6
misostack <misostack.com@gmail.com>
2020-07-10 11:29:22 +0700

    Authentication vs Authorization

--- postgresql/localhost-dbdesign Session.sql
+++ postgresql/localhost-dbdesign Session.sql
@@ -1,21 +1,74 @@
-CREATE TYPE admin_user_roles AS ENUM ('sadmin', 'admin');
+-- BEGIN;
 
-CREATE TABLE IF NOT EXISTS admin_user (
-  id uuid DEFAULT uuid_generate_v4(),
-  email VARCHAR(360) NOT NULL,
-  first_name VARCHAR(60) NOT NULL,
-  last_name VARCHAR(60) NOT NULL,
-  user_roles admin_user_roles[] NOT NULL,
-);
+-- -- ADD UUID EXTENSION
+-- CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- ALTER TABLE admin_user 
+-- DO $$
+-- BEGIN
+--   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'admin_user_role') THEN
+--       CREATE TYPE admin_user_role AS ENUM ('sadmin', 'admin', 'supervisor');
+--   END IF;
+-- END
+-- $$;
 
-CREATE TABLE IF NOT EXISTS example (
-  id uuid DEFAULT uuid_generate_v4(),
-  a_boolean BOOLEAN NOT NULL,
-  the_char CHAR(1) NOT NULL,
-  the_varchar VARCHAR(20) NOT NULL,
-  the_text TEXT NULL DEFAULT "",
-  the_date DATE NOT NULL,
-  
-)
+-- DO $$
+-- BEGIN
+--   IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'admin_user_status') THEN
+--       CREATE TYPE admin_user_status AS ENUM ('active', 'inactive');
+--   END IF;
+-- END
+-- $$;
+
+-- DROP TABLE IF EXISTS admin_user;
+
+-- CREATE TABLE IF NOT EXISTS
+-- admin_user (
+--   id uuid DEFAULT uuid_generate_v4(),
+--   email VARCHAR(360) NOT NULL,
+--   first_name VARCHAR(60) NOT NULL,
+--   last_name VARCHAR(60) NOT NULL,
+--   password VARCHAR(60) NOT NULL,
+--   role admin_user_role NOT NULL,
+--   status admin_user_status NULL DEFAULT 'active'
+-- );
+
+-- ALTER TABLE admin_user
+-- ADD PRIMARY KEY(id);
+
+-- ALTER TABLE table_name DROP CONSTRAINT primary_key_constraint;
+
+-- CREATE UNIQUE INDEX CONCURRENTLY admin_user_email 
+-- ON admin_user (email);
+
+-- ALTER TABLE admin_user
+-- ADD CONSTRAINT unique_email
+-- UNIQUE USING INDEX admin_user_email;
+
+-- COMMIT;
+
+CREATE TABLE t_data (a int, b int, c int);
+
+INSERT INTO t_data 
+        SELECT random()*100000, 
+            random()*100000, 
+            random()*100000 
+        FROM generate_series(1, 1000000);
+
+-- CREATE INDEX idx_data ON t_data(a, b, c);
+
+CREATE INDEX idx_data ON t_data(c);
+
+ANALYZE t_data;
+
+explain analyze SELECT * 
+       FROM   t_data 
+       WHERE  c = 10 
+              AND b = 20 
+              AND a = 10;
+
+
+-- Gather (cost=1000.00..13697.77 rows=1 width=12)	
+-- Workers Planned: 2	
+-- -> Parallel Seq Scan on t_data (cost=0.00..12697.67 rows=1 width=12)	
+-- Filter: ((c = 10) AND (a = 10) AND (b = 20))
+
---
+++ system-design/README.md
@@ -0,0 +1,15 @@
+# System Design
+
+## Keywords
+
+> 1. Authentication vs Authorization
+
+References:
+
+- https://auth0.com/docs/authorization/concepts/authz-and-authn
+- https://medium.com/datadriveninvestor/authentication-vs-authorization-716fea914d55
+
+Simply:
+
+> Authentication : who are you?
+> Authorization : what can you do? ( permissions to access a resource )
